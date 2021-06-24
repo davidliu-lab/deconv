3228f87e57e71ed442f6178a4b358345ad70079b
William Grisaitis <wgrisaitis@gmail.com>
2021-06-24 23:12:34 +0000

    Add helpers package

---
+++ helpers/__init__.py
@@ -0,0 +1,1 @@
+from .download_ftp_file import download_gz_from_ftp
---
+++ helpers/download_ftp_file.py
@@ -0,0 +1,23 @@
+from contextlib import closing
+import gzip
+import os
+import shutil
+import urllib.request as request
+
+
+def download_gz_from_ftp(ftp_url, base_dir):
+    # base_dir = './data/downloaded_manually'
+    # ftp_url = 'ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE127nnn/GSE127813/suppl/GSE127813_Whole_blood_gene_expression_matrix.txt.gz'
+
+    filename = os.path.basename(ftp_url)
+    filepath = os.path.join(base_dir, filename)
+
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+
+    with open(filepath, 'wb') as f, closing(request.urlopen(ftp_url)) as r:
+        shutil.copyfileobj(r, f)
+
+    with gzip.open(filepath, 'rb') as f_in:
+        filepath_txt = os.path.splitext(filepath)[0]  # remove .gz extension
+        with open(filepath_txt, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
