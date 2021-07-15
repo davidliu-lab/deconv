1c3f7251136f58fd2c66b2a17dc377506725abf4
William Grisaitis <wgrisaitis@gmail.com>
2021-07-15 16:13:10 +0000

    run csx on tirosh melanoma data (tutorial 1)

---
+++ scripts/tut1-csx_fractions_melanoma.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# variables
+MIXTURE_FILE="/mnt/buckets/liulab/csx_example_files/Single_Cell_RNA-Seq_Melanoma_SuppFig_3b-d/mixture_melanoma_Tirosh_SuppFig_3b-d.txt"
+REFSAMPLE_FILE="/mnt/buckets/liulab/csx_example_files/Single_Cell_RNA-Seq_Melanoma_SuppFig_3b-d/scRNA-Seq_reference_melanoma_Tirosh_SuppFig_3b-d.txt"
+
+
+# setup
+CSX_DIR="/mnt/buckets/liulab/csx-runs/$(date '+%Y%m%d_%H%M%S')"
+echo "setting up CIBERSORTx in directory:"
+echo $CSX_DIR
+mkdir -p $CSX_DIR/in $CSX_DIR/out
+cp $MIXTURE_FILE $CSX_DIR/in/mymixture.txt
+cp $REFSAMPLE_FILE $CSX_DIR/in/myrefsample.txt
+
+## verify setup
+tree -h $CSX_DIR
+
+# run csx
+docker run \
+    --rm \
+    -v $CSX_DIR/in:/src/data \
+    -v $CSX_DIR/out:/src/outdir \
+    --user "$(id -u):$(id -g)" \
+    cibersortx/fractions:latest \
+    --username lyronctk@stanford.edu \
+    --token dfeba2c8b9d61daebee5fa87026b8e56 \
+    --single_cell TRUE \
+    --refsample myrefsample.txt \
+    --mixture mymixture.txt \
+    --rmbatchSmode TRUE \
+    --verbose TRUE
+
+#     --perm 10 \
+#     --fraction 0 \
+#     --sourceGEPs signature_matrix.txt
+
+tree -h $CSX_DIR
