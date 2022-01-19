093f9228e8073ea08537a3b67e12b1db2693d504
William Grisaitis <wgrisaitis@gmail.com>
2022-01-19 22:43:49 +0000

    finish cohort gen helper, demo in notebook

--- helpers/creating_mixtures.py
+++ helpers/creating_mixtures.py
@@ -2,22 +2,7 @@ import numpy as np
 import pandas as pd
 
 
-def make_cohort(
-    sc_data: pd.DataFrame,
-    sc_metadata: pd.DataFrame,
-    n_samples: int,
-    n_cells_per_gep: int,
-    rng: np.random.RandomState,
-):
-    samples = pd.Series(
-        [f"sample_{j:0{len(str(n_samples))}d}" for j in range(n_samples)],
-        name="Mixture",
-    )
-    cell_types = list(sorted(sc_metadata["cell.type"].unique()))
-    n_cell_types = len(cell_types)
-    fraction_values = rng.dirichlet(alpha=(0.5,) * n_cell_types, size=(n_samples,))
-    fractions = pd.DataFrame(fraction_values, index=samples, columns=cell_types)
-
+def make_a_cell_type_gep_matrix(sc_data, sc_metadata, n_cells_per_gep, rng):
     # for one in silico mixture...
     sampled_single_cells_per_type = (
         sc_metadata.groupby("cell.types")
@@ -31,6 +16,32 @@ def make_cohort(
         },
         axis="columns",
     )
-    mixtures = pd.concat()
+    cell_type_geps *= 100_000 / cell_type_geps.sum()
+    return cell_type_geps
 
+
+def make_mixtures(
+    sc_data: pd.DataFrame,
+    sc_metadata: pd.DataFrame,
+    n_samples: int,
+    n_cells_per_gep: int,
+    rng: np.random.RandomState,
+):
+    samples = pd.Series(
+        [f"sample_{j:0{len(str(n_samples))}d}" for j in range(n_samples)],
+        name="Mixture",
+    )
+    cell_types = list(sorted(sc_metadata["cell.types"].unique()))
+    fraction_values = rng.dirichlet(alpha=(0.5,) * len(cell_types), size=(n_samples,))
+    fractions = pd.DataFrame(fraction_values, index=samples, columns=cell_types)
+    cell_type_geps = {
+        sample: make_a_cell_type_gep_matrix(sc_data, sc_metadata, n_cells_per_gep, rng)
+        for sample in samples
+    }
+    mixtures = pd.concat(
+        {sample: cell_type_geps[sample] @ fractions.loc[sample] for sample in samples},
+        axis=1,
+    )
+    mixtures *= rng.uniform(0.9, 1.1, size=(mixtures.shape))
+    mixtures *= 100_000 / mixtures.sum()
     return mixtures, fractions, cell_type_geps
--- helpers/loading_single_cell_cohorts.py
+++ helpers/loading_single_cell_cohorts.py
@@ -3,8 +3,12 @@ import pandas as pd
 from helpers.cell_type_naming import weird_to_nice
 
 
-def load_tirosh():
-    sc_rna_seq = pd.read_csv("gs://liulab/ftp/GSE115978/GSE115978_tpm.csv", index_col=0)
+def load_tirosh(n_genes_if_not_all=None):
+    sc_rna_seq = pd.read_csv(
+        "gs://liulab/ftp/GSE115978/GSE115978_tpm.csv",
+        index_col=0,
+        nrows=n_genes_if_not_all,
+    )
     sc_rna_seq = sc_rna_seq.rename_axis(index="GeneSymbol", columns="cells")
 
     metadata = pd.read_csv("gs://liulab/ftp/GSE115978/GSE115978_cell.annotations.csv")
