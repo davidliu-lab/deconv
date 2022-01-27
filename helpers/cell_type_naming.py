57f0f4540b0e85167b2f9de14f1a843fee1212d6
William Grisaitis <wgrisaitis@gmail.com>
2022-01-27 05:43:42 +0000

    add bad cell names

--- helpers/cell_type_naming.py
+++ helpers/cell_type_naming.py
@@ -1,17 +1,20 @@
 nice_to_weirds = {
-    'Malignant': ['Malignant.cell', 'Mal'],
-    'Endothelial': ['Endothelial.cell', 'Endothelial cells', 'Endo.'],
-    'CAF': [],
-    'T CD8': ['T.CD8', 'T cells CD8', 'TCD8'],
-    'NK': ['NK cells'],
-    'Macrophage': ['Macrophages'],
-    'T CD4': ['T.CD4', 'T cells CD4', 'TCD4'],
-    'B': ['B.cell', 'B cells'],
-    'T': ['T.cell'],
+    "Malignant": ["Malignant.cell", "Mal", "Malignant cell"],
+    "Endothelial": [
+        "Endothelial.cell",
+        "Endothelial cells",
+        "Endo.",
+        "Endothelial cell",
+    ],
+    "CAF": [],
+    "T CD8": ["T.CD8", "T cells CD8", "TCD8"],
+    "NK": ["NK cells"],
+    "Macrophage": ["Macrophages"],
+    "T CD4": ["T.CD4", "T cells CD4", "TCD4"],
+    "B": ["B.cell", "B cells", "B cell"],
+    "T": ["T.cell", "T cell"],
 }
 
 weird_to_nice = {
-    weird: nice
-    for nice in nice_to_weirds
-    for weird in nice_to_weirds[nice]
+    weird: nice for nice in nice_to_weirds for weird in nice_to_weirds[nice]
 }
