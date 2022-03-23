31f2f9610824a93e528d1ee818d1353070e36408
William Grisaitis <wgrisaitis@gmail.com>
2022-03-23 19:49:17 +0000

    improve CSV reading: use pyarrow engine to speed things up, add logging

--- helpers/datasets.py
+++ helpers/datasets.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Tuple
 
 import pandas as pd
@@ -5,6 +6,7 @@ import pandas as pd
 from helpers import columns
 from helpers.cell_type_naming import weird_to_nice
 
+logger = logging.getLogger(__name__)
 
 def load_jerby_arnon(n_genes: int = None) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Load Jerby-Arnon scRNA-seq data
@@ -15,14 +17,17 @@ def load_jerby_arnon(n_genes: int = None) -> Tuple[pd.DataFrame, pd.DataFrame]:
     Returns:
         tuple: two dataframes, one each of sc data and metadata
     """
+    logger.debug("loading Jerby-Arnon scRNA-seq data")
     sc_rna_seq = pd.read_csv(
         "gs://liulab/ftp/GSE115978/GSE115978_tpm.csv",
         index_col=0,
         nrows=n_genes,
+        engine="pyarrow",
     )
     sc_rna_seq = sc_rna_seq.rename_axis(index=columns.GENE_SYMBOL, columns=columns.SINGLE_CELL_ID)
     sc_rna_seq = sc_rna_seq.sort_index(axis="columns")
     sc_rna_seq = sc_rna_seq.sort_index(axis="rows")
+    logger.debug("loading Jerby-Arnon metadata")
     metadata = pd.read_csv(
         "gs://liulab/ftp/GSE115978/GSE115978_cell.annotations.csv",
         na_values={"cell.types": "?"},
@@ -43,8 +48,9 @@ def load_tcga_skcm(n_genes: int = None) -> pd.DataFrame:
     Returns:
         mixtures_tcga_skcm: pandas.DataFrame
     """
+    logger.debug("loading TCGA SKCM bulk RNA-seq data")
     path = "gs://liulab/downloaded_manually/derek_csx_tcga_skcm/skcm_rnaseqv2_normalized_clean.txt"
-    bulk_rna_seq = pd.read_csv(path, sep="\t", index_col=0, nrows=n_genes)
+    bulk_rna_seq = pd.read_csv(path, sep="\t", index_col=0, nrows=n_genes, engine="pyarrow")
     # clean up index (gene symbols)
     bulk_rna_seq = bulk_rna_seq.sort_index()
     # clean up columns (sample IDs)
@@ -56,6 +62,7 @@ def load_tcga_skcm(n_genes: int = None) -> pd.DataFrame:
 
 
 def load_tcga_skcm_fractions_from_csx() -> pd.DataFrame:
+    logger.debug("loading TCGA SKCM fractions estimated by CIBERSORTx")
     path = "gs://liulab/downloaded_manually/derek_csx_tcga_skcm/CIBERSORTx_Job8_Results.txt"
     fractions = pd.read_csv(path, sep="\t", index_col=0)
     # clean up index (sample IDs)
