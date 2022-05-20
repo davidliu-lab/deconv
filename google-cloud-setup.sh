e75de109654ef76886abe5e0d747bb8c1548e4b0
William Grisaitis <wgrisaitis@gmail.com>
2022-05-20 23:34:06 +0000

    update machine setup script

--- google-cloud-setup.sh
+++ google-cloud-setup.sh
@@ -1,4 +1,24 @@
 sudo apt update
-sudo apt install -y tree
-conda install -n base -c conda-forge mamba 
-mamba install -n base -c conda-forge black isort jupyterlab_code_formatter
+sudo apt install -y \
+    pandoc \
+    tree \
+    texlive-xetex \
+    texlive-fonts-recommended \
+    texlive-plain-generic
+
+curl https://raw.githubusercontent.com/GitAlias/gitalias/master/gitalias.txt -o ~/.gitalias 
+git config --global include.path ~/.gitalias
+
+conda install -y -n base -c conda-forge mamba
+mamba install -y -n base -c conda-forge \
+    black \
+    isort \
+    jupyter-book \
+    jupyterlab_code_formatter \
+    jupyterlab-lsp \
+    jupyterlab_miami_nights \
+    nbconvert \
+    pre-commit \
+    python-lsp-server
+
+mamba env create --file conda-env.yml
