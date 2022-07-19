f7ca4aeeca323a72761f002916405f218ebe64ca
William Grisaitis <wgrisaitis@gmail.com>
2022-07-19 17:17:41 +0000

    fix copying from local to gcs, with full directory paths

--- helpers/running_cibersortx/copying_to_gcs.py
+++ helpers/running_cibersortx/copying_to_gcs.py
@@ -1,24 +1,30 @@
 import glob
+import logging
 import os
 import pathlib
 import shutil
+import time
 
-from cloudpathlib import AnyPath
+from cloudpathlib import AnyPath, GSPath
 from google.cloud import storage
+
+import helpers
+
+logger = logging.getLogger(__name__)
 
 
 def copy_local_directory_to_gcs(local_path: str, bucket: storage.Bucket, gcs_path: str):
-    """Recursively copy a directory of files to GCS.
-
-    local_path should be a directory and not have a trailing slash.
-    """
-    assert os.path.isdir(local_path)
-    for local_file in glob.glob(local_path + "/**"):
-        if not os.path.isfile(local_file):
+    path = pathlib.Path(local_path)
+    for i in path.glob("**/*"):
+        if i.is_dir():
             continue
-        remote_path = os.path.join(gcs_path, local_file[1 + len(local_path) :])
-        blob = bucket.blob(remote_path)
-        blob.upload_from_filename(local_file)
+        t = time.time()
+        target_gcs_path = GSPath(gcs_path) / i.relative_to(local_path)
+        blob = bucket.blob(target_gcs_path.blob)
+        blob.upload_from_filename(i.resolve())
+        logger.debug(
+            f"Took {time.time() - t:4.3f} seconds to upload {i} to {target_gcs_path}"
+        )
 
 
 def copy_file_maybe_in_the_cloud_to_local_path(
@@ -29,3 +35,11 @@ def copy_file_maybe_in_the_cloud_to_local_path(
         path.copy(target_path)
     except AttributeError:
         shutil.copy(path.resolve(), target_path.resolve())
+
+
+if __name__ == "__main__":
+    logger.setLevel("DEBUG")
+    helpers.logging.configure_logging()
+    bucket = storage.Client().bucket("liulab")
+    local_dir = "/home/jupyter/deconv/tmp"
+    copy_local_directory_to_gcs(local_dir, bucket, gcs_path="gs://liulab/tmp/stuff2/")
