1c51789a10c0c1c8a3556d6bd5fee16a6b5fc72a
William C Grisaitis <wgrisaitis@gmail.com>
2022-07-19 10:38:52 +0200

    add main script in creating_input_files for making refsample from all of jerby-arnon

--- helpers/running_cibersortx/creating_input_files.py
+++ helpers/running_cibersortx/creating_input_files.py
@@ -1,4 +1,8 @@
+import logging
+
 import pandas as pd
+
+import helpers.columns
 
 
 def create_csx_mixture_tsv(df_bulk_rnaseq: pd.DataFrame, uri_file: str) -> str:
@@ -31,3 +35,28 @@ def create_csx_refsample_tsv(
     df_refsample_sc_rnaseq = df_refsample_sc_rnaseq.reset_index()
     df_refsample_sc_rnaseq.to_csv(uri_file, index=False, sep="\t")
     return uri_file
+
+
+if __name__ == "__main__":
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+    handler.setFormatter(formatter)
+    logging.getLogger().addHandler(handler)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    logger.debug("loading sc data")
+    (
+        df_sc_rnaseq,
+        df_metadata,
+    ) = helpers.datasets.load_jerby_arnon_hg19_tpm()
+    logger.debug(f"length of df_jerby_arnon_sc_rnaseq: {len(df_sc_rnaseq)}")
+    nonnull_cell_type_cells = ~df_metadata[helpers.columns.CELL_TYPE].isna()
+    df_sc_rnaseq = df_sc_rnaseq.loc[nonnull_cell_type_cells]
+    logger.debug(f"new length of df_jerby_arnon_sc_rnaseq: {len(df_sc_rnaseq)}")
+    logger.debug("creating and writing refsample tsv")
+    uri_file = "gs://liulab/tmp/refsample_from_helpers.tsv"
+    create_csx_refsample_tsv(df_sc_rnaseq, df_metadata, uri_file)
+    logger.debug("validating refsample tsv")
+    df = pd.read_csv(uri_file, sep="\t")
+    logger.debug(f"length of df: {len(df)}")
+    print("\n".join(df.columns))
