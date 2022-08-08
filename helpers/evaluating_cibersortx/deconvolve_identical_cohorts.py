ef5b89e778010d9fd3ffdef9ea1fd8da09841fea
William C Grisaitis <wgrisaitis@gmail.com>
2022-08-08 15:35:21 -0400

    WIP some hires analyses

---
+++ helpers/evaluating_cibersortx/deconvolve_identical_cohorts.py
@@ -0,0 +1,37 @@
+import cloudpathlib
+import pandas as pd
+
+from helpers import creating_mixtures, datasets, running_cibersortx
+
+sc_data, sc_metadata = datasets.load_jerby_arnon(ref_genome="hg19", units="tpm")
+tcga_skcm_fractions = datasets.load_tcga_skcm_fractions_from_csx()
+tcga_skcm_metastatic_sample_barcodes = (
+    datasets.load_tcga_skcm_metastatic_sample_barcodes()
+)
+tcga_skcm_fractions_mets = NotImplemented
+
+
+def run_trial(
+    sc_data: pd.DataFrame,
+    sc_metadata: pd.DataFrame,
+    fractions: pd.DataFrame,
+    trial_id: int,
+) -> None:
+    path_data_prefix = cloudpathlib.AnyPath(f"gs://liulab/evaluating_cibersortx/identical_cohorts/{trial_id}")
+    df_pseudobulks = creating_mixtures.make_mixtures(
+        sc_data, sc_metadata, tcga_skcm_fractions_mets
+    )
+    uri_pseudobulks = str()
+    running_cibersortx.creating_input_files.create_csx_mixtures_tsv(
+        df_pseudobulks, uri_pseudobulks
+    )
+    uri_results_fractions = str(path_results_prefix / "csx_results" / "fractions")
+    # uri_results_hires = f"gs://liulab/evaluating_cibersortx/identical_cohorts/{trial_id}/results_hires"
+    uri_results_hires = str(path_results_prefix / "csx_results" / "hires")
+    running_cibersortx.sigmatrix_and_fractions.run_fractions_and_upload(uri_pseudobulks, uri_refsample_sc_rnaseq, uri_results_fractions)
+    running_cibersortx.hires_with_fractions.run_and_upload(
+        uri_save_job_files_to=,
+        uri_bulk_rnaseq=uri_pseudobulks,
+        uri_sigmatrix=uri_sigmatrix,
+        uri_sourcegeps=uri_sourcegeps,
+    )
