a245c22c5359ff0f34ee0a9d3dc21f0c97d537e7
William C Grisaitis <wgrisaitis@gmail.com>
2022-09-14 23:38:37 -0400

    make volcano plot of negative countrol, malignant cells only

--- helpers/running_cibersortx/__init__.py
+++ helpers/running_cibersortx/__init__.py
@@ -3,5 +3,6 @@ from . import (
     creating_input_files,
     hires_only,
     hires_with_fractions,
+    reading_output_files,
     sigmatrix_and_fractions,
 )
--- helpers/running_cibersortx/reading_output_files.py
+++ helpers/running_cibersortx/reading_output_files.py
@@ -5,23 +5,24 @@ import cloudpathlib
 import dask.dataframe as dd
 
 
-def read_hires_cell_type_geps(path: Union[pathlib.Path, cloudpathlib.CloudPath]):
-    """Read the high-resolution cell type gene expression profiles from a file.
+def read_hires_cell_type_geps(
+    path_pattern: Union[pathlib.Path, cloudpathlib.CloudPath]
+):
+    """Read cell type-specific gene expression outputs from CIBERSORTx hires mode.
 
     Parameters
     ----------
-    path : pathlib.Path or cloudpathlib.CloudPath
-        The path to the file to read.
+    path_pattern : pathlib.Path or cloudpathlib.CloudPath
+        path with wildcards to file(s) to read.
 
     Returns
     -------
     pandas.DataFrame
         The high-resolution cell type gene expression profiles.
     """
-    path_pattern = path / "outdir" / "CIBERSORTxHiRes_NA_*_Window*txt"
     df = dd.read_csv(str(path_pattern), sep="\t", include_path_column=True).compute()
     df[["cell_type"]] = df["path"].str.extract(
-        ".*/outdir/CIBERSORTxHiRes_NA_(.*)_Window.*\.txt"
+        ".*/CIBERSORTxHiRes_NA_(.*)_Window.*\.txt"
     )
     df = df.drop(columns="path")
     df = df.pivot(index=["cell_type", "GeneSymbol"], columns=[])
@@ -38,5 +39,6 @@ if __name__ == "__main__":
     path = cloudpathlib.CloudPath(
         "gs://liulab/evaluating_cibersortx/perturbed_gene_expression/2x/2022-09-14_15:17:34"
     )
-    df = read_hires_cell_type_geps(path)
+    path_pattern = path / "outdir" / "CIBERSORTxHiRes_NA_*_Window*txt"
+    df = read_hires_cell_type_geps(path_pattern)
     print(df)
