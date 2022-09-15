b1ba9a2701fc3fc86ed4728c747e3fa45a780c5a
William C Grisaitis <wgrisaitis@gmail.com>
2022-09-15 00:58:00 -0400

    make timestamp more file system friendly

--- helpers/useful_small_things.py
+++ helpers/useful_small_things.py
@@ -2,4 +2,4 @@ import datetime
 
 
 def make_a_nice_timestamp_of_now() -> str:
-    return datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    return datetime.datetime.now().strftime("%Y%m%d_%Hh%Mm%Ss")
