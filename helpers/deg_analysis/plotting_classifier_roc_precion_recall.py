150fb9ac20b23f57169e42b6e679f9051770be81
grisaitis <wgrisaitis@gmail.com>
2023-06-05 15:49:40 +0000

    improve formatting for score tables

--- helpers/deg_analysis/plotting_classifier_roc_precion_recall.py
+++ helpers/deg_analysis/plotting_classifier_roc_precion_recall.py
@@ -115,20 +115,56 @@ def plot_metric_by_threshold(
     return fig
 
 
-def make_table_scores(df_scores: pd.DataFrame) -> pd.DataFrame:
-    return (
-        df_scores.groupby(["malignant_means", "log2_fc"]).mean().unstack("log2_fc")
-    ).style.background_gradient(cmap="RdBu", axis=None, vmin=-0.5, vmax=1.5)
+def make_score_table(scores: pd.Series, cmap: str = "RdBu") -> pd.DataFrame:
+    # if some float type
+    if scores.dtype == float:
+        cmap = cmap or "RdBu"
+        return (
+            scores.groupby(["malignant_means", "log2_fc"]).mean().unstack("log2_fc")
+        ).style.background_gradient(cmap=cmap, axis=None, vmin=-0.5, vmax=1.5)
+    # if int
+    elif scores.dtype == int:
+        cmap = cmap or "Blues"
+        df = (
+            scores.groupby(["malignant_means", "log2_fc"]).mean().unstack("log2_fc")
+        ).style.background_gradient(cmap=cmap, axis=None, vmin=0, vmax=500)
+        df = df.style.format("{:.1f}")
+        return df
 
 
-def make_table_scores_with_stddev(df_scores: pd.DataFrame) -> pd.DataFrame:
+def make_score_table_with_stddev(scores: pd.Series, cmap: str = "RdBu") -> pd.DataFrame:
+    aggfuncs = ["mean", "std"]
+    aggregations = scores.groupby(["malignant_means", "log2_fc"]).agg(func=aggfuncs)
+    means_and_stddevs = aggregations.apply(
+        lambda row: f"{row['mean']:4.2f}±{row['std']:4.2f}", axis="columns"
+    )
+    df_means = aggregations["mean"].unstack("log2_fc")
+    df_means_and_stddevs = means_and_stddevs.unstack("log2_fc")
+    return df_means_and_stddevs.style.background_gradient(
+        cmap=cmap, axis=None, vmin=-0.5, vmax=1.5, gmap=df_means
+    )
+
+
+def make_score_table_with_stddev_1(scores: pd.Series, cmap: str = "RdBu") -> pd.DataFrame:
     def mean_and_stddev(series: pd.Series):
-        mean = series.mean()
-        stddev = series.std()
-        return f"{mean:4.2f}±{stddev:4.2f}"
+        return f"{series.mean():4.2f}±{series.std():4.2f}"
 
-    return (
+    aggfuncs = {
+        "mean": "mean",
+        "mean_and_stddev": mean_and_stddev,
+    }
+    aggregations = (
         df_scores.groupby(["malignant_means", "log2_fc"])
-        .agg(func=mean_and_stddev)
+        .agg(func=aggfuncs)
+        .rename_axis(columns="aggregation")
         .unstack("log2_fc")
-    ).style.background_gradient(cmap="Blues", axis=None, vmin=0.0, vmax=1.0)
+    )
+    means = aggregations.xs("mean", level=1, axis="columns")
+    means_and_stddevs = aggregations.xs("mean_and_stddev", level=1, axis="columns")
+    return means_and_stddevs.style.background_gradient(
+        cmap=cmap,
+        axis=None,
+        vmin=0.0,
+        vmax=1.0,
+        gmap=means,
+    )
